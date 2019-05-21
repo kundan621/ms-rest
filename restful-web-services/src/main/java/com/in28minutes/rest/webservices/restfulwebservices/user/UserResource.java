38effed1169521c61e592b7352360fff40be9c23
kundan621 <kundan621@gmail.com>
2019-05-21 07:46:45 +0530

    video 21

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,13 +1,17 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriBuilder;
 
 @RestController
 public class UserResource {
@@ -30,10 +34,12 @@ public class UserResource {
 	
 	//add user
 	@PostMapping("/users")
-	public void addUser(@RequestBody User user)
+	public ResponseEntity<Object> addUser(@RequestBody User user)
 	{
 	
 		User savedUser=service.save(user);
+		URI location= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 	
 }
